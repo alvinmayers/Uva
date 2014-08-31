a0bbe327461766af779d86c9f95015c430dff577
Alvin Mayers <alvin@alvincode.com>
2014-08-31 02:00:03 -0400

    initial_commit

---
+++ vol_011_1100-1199/1124_Celebrity-Jeopardy.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * @problem uva-1124  CELEBRITY JEOPARDY
+ * @author Alvin Dean Jr.
+ * @run_time 0.019 sec
+ */
+
+int main(){
+	char c; 
+	while((c=getchar())!=EOF) putchar(c); 
+}
